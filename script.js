7b63a93c0325ec11fc01801a973373bc53e95b92
AxelBroeglin <axelbroeglin@gmail.com>
2022-07-14 17:22:18 +0100

    Data test

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const dataList = document.getElementById('weight');
+
+let data = JSON.parse(activities);
+console.log(data[0]);
