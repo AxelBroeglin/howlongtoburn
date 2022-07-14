fa70f775307f81d15d5d6ea82c760cb7a0cd2490
AxelBroeglin <axelbroeglin@gmail.com>
2022-07-14 23:30:05 +0100

    Autocompletion is working

--- script.js
+++ script.js
@@ -1,41 +1,82 @@
-const main = document.getElementById('main');
-const form = document.getElementById('form');
+// const main = document.getElementById('main');
+// const form = document.getElementById('form');
+// const search = document.getElementById('search');
+// const dataList = document.getElementById('weight');
+
+
+// function getActivities(filteredActivities){
+//     fetch("./activities.json")
+//     .then(response => {
+//        return response.json();
+//     })
+//     .then(data => console.log(filteredActivities));
+//     // .then(data => data.forEach(object =>{
+//     //     if(object.activity === "filteredActivities"){
+//     //         console.log(object[1]);
+//     //     }else{
+//     //         console.log('bah non')
+//     //     }
+//     // }));
+// }
+
+// function getMovies(url){
+//     fetch(url).then(res => res.json()).then(data => {
+//         showActivities(data.results);
+//         }    
+//     )
+// }
+
+// function showActivities(){
+//     //append input Myactivity
+//     //Dynamic generation
+// }
+
+
+// form.addEventListener("keyup", e => {
+//     let searchString = [];
+//     searchString.push(e.target.value.toLowerCase());
+//     console.log(searchString)
+//     const filteredActivities = searchString.filter(activity => {
+//     return (
+//         activity.includes(searchString)
+//     );
+//   });        
+
+//   getActivities(filteredActivities);
+//   showActivities(filteredActivities);
+//   //Erase if nothing written
+// });
+
+
 const search = document.getElementById('search');
-const dataList = document.getElementById('weight');
+const matchList = document.getElementById('match-list');
 
+//Search JSON and filter it
+const searchActivities = async searchText => {
+    const res = await fetch('./activities.json');
+    const activities = await res.json();
+    //Get matches to current text input
+    let matches = activities.filter(sport => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return sport.Activity.match(regex);
+    });
 
-function getActivities(filteredActivities){
-    fetch("./activities.json")
-    .then(response => {
-       return response.json();
-    })
-    .then(data => showActivities(data.results));
-}
+    if (searchText.length === 0){
+        matches = [];
+    }
 
-function showActivities(filteredActivities){
-    //append input Myactivity
-    //Dynamic generation
-}
+    console.table(matches);
+    //outputHtml(matches);
+};
 
+//Show results in HTML
+const outputHtml = matches => {
+    if(matches.length > 0){
+        const html = matches.map(match => `<p>${match}</p>`)
+        .join('');
+        matchList.innerHTML = html;
+        console.log(html);
+    }
+};
 
-form.addEventListener("keyup", e => {
-    let searchString = [];
-    searchString.push(e.target.value.toLowerCase());
-
-        console.log(searchString);
-    
-    const filteredActivities = searchString.filter(activity => {
-
-    return (
-        activity.includes(searchString)
-    );
-    
-  });
-  console.log(filteredActivities)
-  getActivities(filteredActivities);
-
-  showActivities(filteredActivities);
-
-  //Erase if nothing written
-
-});
+search.addEventListener('input', () => searchActivities(search.value));
