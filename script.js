925f351992f71a80648d22abfebb611a05c35d5c
AxelBroeglin <axelbroeglin@gmail.com>
2022-08-12 15:12:14 +0100

    Commented code

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const spanActivity = document.getElementById('span-activity');
 const spanMinutes = document.getElementById('span-minutes');
 
 
-/*******************************************/
-/***************FOOD CODE*******************/
-/*****************************************/
+/***********************
+ * *FOOD CODE **********
+ **********************/
 
 //Get data from api from search
 const searchForFood = async searchTextFood => {
@@ -27,12 +27,13 @@ const searchForFood = async searchTextFood => {
         },
         });
     let nutriments = await res.json();
-    nutriments=Array.from(nutriments.branded);    
+    //Make array from object
+    nutriments=Array.from(nutriments.branded);
     outputHtmlFood(nutriments);
 };
 
 
-//Show results in HTML
+//Show food results in HTML
 const outputHtmlFood = nutriments => {
     let html = [];
     for (let i = 0; i < 10; i++) {
@@ -41,25 +42,25 @@ const outputHtmlFood = nutriments => {
     }
 }
 
-let weightOptions = ["<option></option>"];
-for (let i = 50; i < 121; i++) {
-    weightOptions.push(`<option value='${[i]}'> ${[i]} kgs</option>`);
-    weight.innerHTML = weightOptions.join('');  
-    }
 
-/**********FOOD EVENT LISTENERS*********/
+/***********************
+ * *FOOD EVENT LISTENERS
+ **********************/
+
 searchFood.addEventListener('input', (e) =>{
     searchForFood(searchFood.value); 
     }
 );
 
 matchListFood.addEventListener('click', () =>{
+    //Calories = numbers from result from search
     let kcal = event.target.innerHTML.slice(-3);
     if(kcal.length > 0){
         spanCal.classList.add('visible');
         spanCal.classList.remove('invisible');
         spanFood.classList.remove('span-results-default');
         spanFood.classList.add('span-results-filled');
+        //Shows clicked value in the result sentence
         spanFood.innerHTML = `${event.target.innerHTML.slice(0, -6)} (${kcal}`;
         if(weight.value !== "" ||spanFood.classList.contains('span-results-filled')){
             calForKgs();
@@ -67,10 +68,13 @@ matchListFood.addEventListener('click', () =>{
     }
 })
 
-/*******************************************/
-/************ACTIVITIES CODE****************/
-/*****************************************/
 
+
+/***********************
+ * *ACTIVITIES CODE ****
+ **********************/
+
+//Brings info from JSON file
 const searchActivities = async searchText => {
     const res = await fetch('./activities.json');
     const activities = await res.json();
@@ -85,6 +89,7 @@ const searchActivities = async searchText => {
     outputHtml(matches.slice(0,10));
 };
 
+//Generates HTML code with matches
 const outputHtml = matches => {
     if(matches.length > 0){
         const html = matches.map(match => `<div class="search-results" data-kgs60="${match.kgs60}" data-kgs70="${match.kgs70}" data-kgs80="${match.kgs80}" data-kgs90="${match.kgs90}">${match.Activity}</div>`
@@ -94,18 +99,59 @@ const outputHtml = matches => {
     }
 };
 
+
+/*****************************
+ * *ACTIVITIES EVENT LISTENERS
+ ****************************/
+
+//Listens to input in search field
 search.addEventListener('input', () => {
     searchActivities(search.value);
 });
 
+//Generate weight options
+let weightOptions = ["<option></option>"];
+for (let i = 50; i < 121; i++) {
+    weightOptions.push(`<option value='${[i]}'> ${[i]} kgs</option>`);
+    weight.innerHTML = weightOptions.join('');  
+    }
 
-/*******************************************/
-/*************RESULTS CODE*****************/
-/*****************************************/
 
-/*******RESULTS EVENT LISTENERS******/
+/***********************
+ * *RESULT CODE ********
+ **********************/
 
-//if else to check if a calc has already been done, if yes jump to calc, if no, regular calc
+//Gathers info from inputs, send it to calculResults
+const calForKgs = function () {
+    //Takes value of selected weight
+    let selectedWeight = weight.options[weight.selectedIndex].value;
+    //Takes dataset of 60kgs for reference
+    let activityCal = spanActivity.dataset.kgs60;
+    //Calculates proportionnal value
+    let selectedKgs = selectedWeight*activityCal/60;
+    calculResults(selectedKgs);
+}
+
+//Final calc function
+const calculResults = function (selectedKgs) {
+    let kcal = spanFood.innerHTML.slice(-3);
+    //Calculates time to burn
+    let timeToBurn = (kcal/selectedKgs) * 60;
+    if(!isNaN(timeToBurn)){
+        spanMinutes.innerHTML = Math.trunc(timeToBurn);
+        spanMinutes.classList.remove('span-results-default');
+        spanMinutes.classList.add('span-results-filled');
+    } else{
+        spanMinutes.classList.add('span-results-default');
+        spanMinutes.classList.remove('span-results-filled');
+    }
+}
+
+/*************************
+ * *RESULT EVENT LISTENERS
+ ************************/
+
+//Check if a calc has already been done, if yes jumps to calc, if no, regular calc
 matchList.addEventListener('click', () => {
     if(event.target.innerHTML !== ""){
         spanActivity.innerHTML = event.target.innerHTML;
@@ -120,27 +166,7 @@ matchList.addEventListener('click', () => {
     }
 });
  
+//If user changes weight, new calc
  weight.addEventListener('change', () => {
     calForKgs();
  })
-
-
-const calForKgs = function () {
-    let selectedWeight = weight.options[weight.selectedIndex].value;
-    let activityCal = spanActivity.dataset.kgs60;
-    let selectedKgs = selectedWeight*activityCal/60;
-    calculResults(selectedKgs);
-}
-
-const calculResults = function (selectedKgs) {
-    let kcal = spanFood.innerHTML.slice(-3);
-    let timeToBurn = (kcal/selectedKgs) * 60;
-    if(!isNaN(timeToBurn)){
-        spanMinutes.innerHTML = Math.trunc(timeToBurn);
-        spanMinutes.classList.remove('span-results-default');
-        spanMinutes.classList.add('span-results-filled');
-    } else{
-        spanMinutes.classList.add('span-results-default');
-        spanMinutes.classList.remove('span-results-filled');
-    }
-}
